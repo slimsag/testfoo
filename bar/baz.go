5cc2bffea5fa2b2abfb7ce781d431059aa9a3d6f
ggilmore <geoffrey@sourcegraph.com>
2018-06-08 14:01:55 -0700

    comments

--- bar/baz.go
+++ bar/baz.go
@@ -2,10 +2,13 @@ package bar
 
 import "fmt"
 
+// Baz is a struct with contents.
 type Baz struct {
+	// Contents is the slice which contains the contents of the Baz struct.
 	Contents []string
 }
 
+// PrintContents prints the contents of the Baz struct
 func (b *Baz) PrintContents() {
 	fmt.Printf("%v", b.Contents)
 }
